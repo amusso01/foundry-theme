efead74b405832f84dfcf19d09751e0cb7699aba
amusso01 <eandram@hotmail.it>
2018-12-16 15:23:55 +0000

    reinstall npm modules on mac

--- wp-content/themes/foundry/loop-templates/page-work.php
+++ wp-content/themes/foundry/loop-templates/page-work.php
@@ -8,13 +8,32 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+$args = array(
+	'post_type'	=> 'works_post'
+);
 ?>
 
 <section <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<header>
+<?php 
+$works = new WP_Query($args);
+if ($work->have_posts()){
+	while($works->have_posts()){
+		$works->the_post();
+		$post_cats = get_the_category();
+		?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class( 'work-inner', $post_cats );?> >
 
-		
+		</article>
+		<?php
+	}
+}
+	?>
+	
+}
+
+	<header>
 
 	</header><!-- .entry-header -->
    
