2ff172dbe803948225291230160463220320c16e
amusso01 <endram@hotmail.it>
2019-01-04 16:38:09 +0000

    service done

---
+++ wp-content/themes/foundry/page-templates/service-inner.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Template Name: Service-page-inner
+ * 
+ * Template Post Type: page
+ *
+ * This template shows the three services of the website
+ * 
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+?>
+
+<main class="inner-service-pages">
+	<div class="container-fluid inner-service-hero">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-6 text-center ">
+					<?php the_title( '<h1>', '</h1>', true );?>
+				</div>
+				<div class="col-lg-8 text-center">
+					<?php 
+					global $post;
+					$content = $post->post_content;
+		
+					if ( !empty( $content ) ) :
+						echo $content;
+					endif;
+					?>
+					<a href="#" class="btn">GET IN TOUCH</a>
+				</div>
+				<div class="img-fluid text-center col-md-12">
+					<?php the_post_thumbnail( array(900, 400), 'hero image of the page' ) ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php
+	$color = 'gray';
+	$count = 1;
+// check if the repeater field has rows of data
+if( have_rows('service_article') ):
+
+ 	// loop through the rows of data
+	while ( have_rows('service_article') ) : the_row();
+	if($count % 2 == 0){
+		$color = 'gray';
+	?>
+	<div class="container-fluid <?php echo $color; ?>">
+		<div class="container hidden-animate">
+			<div class="row">
+				<div class="col-md-6 image article-box">
+					<img src="<?php the_sub_field('side_image'); ?>" alt="article image">
+				</div>
+				<div class="col-md-6 article-box">
+					<h3><?php the_sub_field('title');?></h3>
+					<p><?php the_sub_field('paragraph');?></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php
+	}else{
+		$color = 'white';
+	?>
+	<div class="container-fluid <?php echo $color; ?>">
+		<div class="container hidden-animate">
+			<div class="row">
+				<div class="col-md-6 article-box">
+					<h3><?php the_sub_field('title');?></h3>
+					<p><?php the_sub_field('paragraph');?></p>
+				</div>
+				<div class="col-md-6 image article-box">
+					<img src="<?php the_sub_field('side_image'); ?>" alt="article image">
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php
+	}
+
+	$count++;
+    endwhile;
+
+else :
+
+    // no rows found
+
+endif;
+
+?>
+		
+</main><!-- main .service-inside -->
+	
+	
+	
+<?php get_footer(); ?>
+	
