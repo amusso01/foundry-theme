0f85480c083bf151febe7bc2492eca1a9045ef95
amusso01 <endram@hotmail.it>
2019-01-24 10:18:12 +0000

    menu

--- wp-content/themes/foundry/inc/class-wp-bootstrap-navwalker.php
+++ wp-content/themes/foundry/inc/class-wp-bootstrap-navwalker.php
@@ -190,7 +190,7 @@ if ( ! class_exists( 'Understrap_WP_Bootstrap_Navwalker' ) ) {
 				$atts['data-hover'] = 'dropdown';
 				$atts['aria-haspopup'] = 'true';
 				$atts['aria-expanded'] = 'false';
-				$atts['class']         = 'nav-link';
+				$atts['class']         = 'nav-link dropdown-toggle';
 				$atts['id']            = 'menu-item-dropdown-' . $item->ID;
 			} else {
 				$atts['href'] = ! empty( $item->url ) ? $item->url : '#';
